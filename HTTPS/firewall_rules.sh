862d5414614468fde1992782f15243ca21e38300
lucian-pdev <lucianpreda@protonmail.com>
2026-04-01 10:14:12 +0300

    testing github sync -- part of attempted fix 2

--- HTTPS/firewall_rules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo ufw default deny incoming
-sudo ufw allow 22/tcp      # SSH
-sudo ufw allow 80/tcp      # HTTP → HTTPS redirect
-sudo ufw allow 443/tcp     # HTTPS
-sudo ufw enable
